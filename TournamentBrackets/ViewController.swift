453e98b608fc14c4814d725db4f0fb38cf6b3c91
edgardo.agno@gmail.com <edgardoagno@EDGARDOs-MacBook-Air.local>
2016-06-02 23:41:26 +0100

    Change Google Ad App Unit ID associated to TournamentBracket bundle ID instead of social tournament which is outdated

--- TournamentBrackets/ViewController.swift
+++ TournamentBrackets/ViewController.swift
@@ -43,7 +43,7 @@ class ViewController: UIViewController {
         #endif
 
         if let bv = bannerView, button = buttonRemoveAdvert where AppObject.sharedInstance?.isAdsShown == true {
-            bannerView.adUnitID = "ca-app-pub-8499873478400384/8183934759" // Live ID
+            bannerView.adUnitID = "ca-app-pub-8499873478400384/2377099953" // Live ID
             bv.rootViewController =  (self.tabBarController) ?? self
             bv.loadRequest(GADRequest())
             button.backgroundColor = UIColor.flatAlizarinColor()
